18a866bd2f9574610b6116423643436f62469e6a
Lynn Guo <lynn.guo1203@gmail.com>
2021-10-17 22:07:54 +0800

    Add function implementation for Chapter 4
    Signed-off-by: Lynn Guo <lynn.guo1203@gmail.com>

--- the_little_schemer.scm
+++ the_little_schemer.scm
@@ -107,3 +107,115 @@
   (lambda (n)
     (+ n 1)))
 
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define tup?
+  (lambda (tup)
+    (cond
+     ((null? tup) #t)
+     ((number? (car tup)) (tup? (cdr tup)))
+     (else #f))))
+
+(define addup
+  (lambda (tup)
+    (cond
+     ((null? tup) 0)
+     (else (+ (car tup)
+	      (addup (cdr tup)))))))
+
+(define x
+  (lambda (n m)
+    (cond
+     ((zero? m) 0)
+     (else (+ n (x n (sub1 m)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+     ((null? tup1) tup2)
+     ((null? tup2) tup1)
+     (else (cons (+ (car tup1) (car tup2))
+		 (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define >
+  (lambda (n m)
+    (cond
+     ((zero? n) #f)
+     ((zero? m) #t)
+     (else (> (sub1 n) (sub1 m))))))
+
+(define <
+  (lambda (n m)
+    (cond
+     ((zero? m) #f)
+     ((zero? n) #t)
+     (else (< (sub1 n) (sub1 m))))))
+
+(define =
+  (lambda (n m)
+    (cond
+     ((> n m) #f)
+     ((< n m) #f)
+     (else #t))))
+
+(define up-arrow
+  (lambda (n m)
+    (cond
+     ((eq? m 1) n)
+     (else (* n (up-arrow n (sub1 m)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+     ((eq? n 1) (car lat))
+     (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((zero? (sub1 n)) (cdr lat))
+     (else (cons (car lat)
+		 (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) '())
+     ((number? (car lat)) (no-nums (cdr lat)))
+     (else (cons (car lat)
+		 (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) '())
+     ((number? (car lat))
+      (cons (car lat)
+	    (all-nums (cdr lat))))
+     (else (all-nums (cdr lat))))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+     ((null? lat) 0)
+     ((eq? a (car lat))
+      (add1 (occur a (cdr lat))))
+     (else (occur a (cdr lat))))))
+
+(define one?
+  (lambda (n)
+    (= n 1)))
+
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? a (car l)) (rember* a (cdr l)))
+       (else (cons (car l) (rember* a (cdr l))))))
+     (else
+      (cons (rember* a (car l))
+	    (rember* a (cdr l)))))))
