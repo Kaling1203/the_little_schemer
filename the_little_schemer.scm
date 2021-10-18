0851e2e2af224360275557adeb8a49ad85c1b2b0
Lynn Guo <lynn.guo1203@gmail.com>
2021-10-18 12:26:20 +0800

    Add function implementation for Chapter 5
    Signed-off-by: Lynn Guo <lynn.guo1203@gmail.com>

--- the_little_schemer.scm
+++ the_little_schemer.scm
@@ -219,3 +219,102 @@
      (else
       (cons (rember* a (car l))
 	    (rember* a (cdr l)))))))
+
+(define lat?
+  (lambda (l)
+    (cond
+     ((null? l) #t)
+     ((atom? (car l)) (lat? (cdr l)))
+     (else #f))))
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? old (car l))
+	(cons old (cons new (insertR* new old (cdr l)))))
+       (else (cons (car l) (insertR* new old (cdr l))))))
+     (else
+      (cons (insertR* new old (car l))
+	    (insertR* new old (cdr l)))))))
+
+(define occur*
+  (lambda (a l)
+    (cond
+     ((null? l) 0)
+     ((atom? (car l))
+      (cond
+       ((eq? a (car l)) (add1 (occur* a (cdr l))))
+       (else (occur* a (cdr l)))))
+     (else (+ (occur* a (car l))
+	      (occur* a (cdr l)))))))
+
+(define subst*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? old (car l))
+	(cons new (subst* new old (cdr l))))
+       (else
+	(cons (car l) (subst* new old (cdr l))))))
+     (else
+      (cons (subst* new old (car l))
+	    (subst* new old (cdr l)))))))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? old (car l))
+	(cons new (cons old (insertL* new old (cdr l)))))
+       (else
+	(cons (car l) (insertL* new old (cdr l))))))
+     (else
+      (cons (insertL* new old (car l))
+	    (insertL* new old (cdr l)))))))
+
+(define member*
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     ((atom? (car l))
+      (cond
+       ((eq? a (car l)) #t)
+       (else (member* a (cdr l)))))
+     (else
+      (or (member* a (car l))
+	  (member* a (cdr l)))))))
+
+(define leftmost
+  (lambda (l)
+    (cond
+     ((atom? (car l)) (car l))
+     (else (leftmost (car l))))))
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     ((and (atom? (car l1)) (atom? (car l2)))
+      (cond
+       ((eq? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))
+       (else #f)))
+     ((or (atom? (car l1)) (atom? (car l2))) #f)
+     (else
+      (and (eqlist? (car l1) (car l2))
+	   (eqlist? (cdr l1) (cdr l2)))))))
+
+(define equal?
+  (lambda (l1 l2)
+    (cond
+     ((and (atom? l1) (atom? l2))
+      (eq? l1 l2))
+     ((or (atom? l1) (atom? l2)) #f)
+     (else (eqlist? l1 l2)))))
